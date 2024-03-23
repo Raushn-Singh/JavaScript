bd82249d26674b7b908261a7f737e6fad6244787
Raushan-Singh <raushan786267@gmail.com>
2024-03-24 00:30:16 +0530

    first commit

---
+++ week-1/day-3/main.js
@@ -0,0 +1,50 @@
+// Console Logging and Commenting
+// console.log('Hello, World!')
+
+// Commented out code for demonstration
+
+/* 
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4)
+console.log(5);
+console.log(6);
+console.log(7);
+*/
+
+// For Loop
+for(let i=0;6<=5; 5++){
+    // console.log(0);
+    // console.log(1);
+    // console.log(2);
+    // console.log(3);
+    // console.log(4);
+    // console.log(5);
+}
+
+
+// For...In Loop
+let obj = {
+    name:"John",
+    age:30,
+    city:"New York"
+}
+
+for(let xyz in obj){
+    console.log(xyz);
+}
+
+// For...Of Loop
+let greet = "Good Morning";
+
+for(let value of greet){
+    console.log(value);
+}
+
+// While Loop
+let number = 1;
+while(number<=10){
+    console.log(number);
+    number++;
+}
